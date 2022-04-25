321460063c8bb282ff039f509616091680967428
Meghana Krishnappa <90026522+meghanakrishnappa1@users.noreply.github.com>
2022-04-25 10:44:27 -0400

    Add files via upload

---
+++ Ex02-Page-Seasons/Ex02-Page-Seasons/MyPageViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MyPageViewController.swift
+//  Ex02-Page-Seasons
+//
+//  Created by Meghana Krishnappa on 4/22/22.
+//
+
+import UIKit
+
+class MyPageViewController: UIPageViewController,UIPageViewControllerDataSource {
+    var controllerArray: [UIViewController]? = nil
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        dataSource = self
+                let storyBoard = UIStoryboard(name: "Main", bundle: nil)
+                let firstVC = storyBoard.instantiateViewController(withIdentifier: "page01")
+                let secondVC = storyBoard.instantiateViewController(withIdentifier: "page02")
+                let thirdVC = storyBoard.instantiateViewController(withIdentifier: "page03")
+                controllerArray = [firstVC, secondVC, thirdVC]
+                self.setViewControllers([controllerArray![0]], direction: UIPageViewController.NavigationDirection.forward, animated: true, completion: nil)
+            }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = controllerArray!.firstIndex(of: viewController) else {
+            return nil
+        }
+        let preIndex = vcIndex - 1
+        guard preIndex >= 0 else {
+            return controllerArray!.last // loops back to end
+        }
+        guard controllerArray!.count > preIndex else { return nil
+        }
+        return controllerArray![preIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let vcIndex = controllerArray!.firstIndex(of: viewController) else {
+            return nil
+        }
+        let nextIndex = vcIndex + 1
+        guard controllerArray!.count != nextIndex else {
+            return controllerArray!.first // loops back to beginning
+        }
+        guard controllerArray!.count > nextIndex else { return nil
+        }
+        return controllerArray![nextIndex]
+    }
+
+}
---
+++ Ex02-Page-Seasons/Ex02-Page-Seasons/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Ex02-Page-Seasons
+//
+//  Created by Meghana Krishnappa on 4/22/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
